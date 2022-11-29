ca123ded6f3bad703282c8e2e95a8fb1fca2910c
Akber-MAC <akberjag@gmail.com>
2022-11-29 12:56:30 +0530

    added a test sh file
    added a test sh file and connected it to the script

--- network_ui/ip_addresses/forms.py
+++ network_ui/ip_addresses/forms.py
@@ -3,7 +3,10 @@ from wtforms import IntegerField, StringField, SubmitField, SelectField
 
 
 class AddForm(FlaskForm):
+    # TODO: Make this field unique
     pc_name = StringField("Enter PC name: ")
+
+    # TODO: Make this field unique
     ip_address = StringField("Enter IP Address: ")
     network_handicap = SelectField("Selecet Network Profile:")
 
--- network_ui/ip_addresses/views.py
+++ network_ui/ip_addresses/views.py
@@ -1,4 +1,4 @@
-import re
+import re, subprocess
 from network_ui import db
 from network_ui.ip_addresses.models import IPAddress
 from network_ui.ip_addresses.forms import AddForm, DeleteForm
@@ -10,14 +10,27 @@ ip_address_blueprint = Blueprint(
 )
 
 
+# this flag is to prevent multiple users from running script simultaniously
+# Ask help for a better implementaion of this.
+is_a_scrip_running = False
+
+
 @ip_address_blueprint.route("/add", methods=["GET", "POST"])
 def add():
+    global is_a_scrip_running
+
     form = AddForm()
     form.network_handicap.choices = [
         (g.id, g.handicap_name) for g in NetworkHandicap.query.all()
     ]
 
+    # if a script is running already take the user to the homescreen and show a flash
+    if is_a_scrip_running:
+        flash("a script is running please wait before adding a new one")
+        return redirect(url_for("index"))
+
     if form.validate_on_submit():
+
         pc_name = form.pc_name.data
         ip_address = form.ip_address.data
         network_handicap = form.network_handicap.data
@@ -26,6 +39,22 @@ def add():
         # https://www.geeksforgeeks.org/python-program-to-validate-an-ip-address/
         regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
         if re.search(regex, ip_address):
+
+            # run the script with subprocess and if it is succesful, add the details to DB
+            is_a_scrip_running = True
+
+            script_call = subprocess.call(
+                [
+                    "bash",
+                    "network_ui/shell_scripts/temp.sh",
+                    str(ip_address),
+                    str(network_handicap),
+                ]
+            )
+
+            if str(script_call) == "0":
+                is_a_scrip_running = False
+
             new_ip_address = IPAddress(pc_name, ip_address, network_handicap)
             db.session.add(new_ip_address)
             db.session.commit()
@@ -35,6 +64,7 @@ def add():
             # TODO: send a flash to the user and dont save the IP
             flash(f"{ip_address} is not a valid IP address")
 
+    # if the network handicap is empty, ask the user to add one first before adding an ip
     if NetworkHandicap.query.all() == []:
         flash("Add a Network Profile first to add an ip address")
         return redirect(url_for("nw_handi.add"))
