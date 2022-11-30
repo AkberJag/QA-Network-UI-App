d14c319cdf455ade71c6920ee27215b30ae761d1
AkberJag <akberjag@gmail.com>
2022-12-01 01:41:05 +0530

    Listing the handicap templates in more readable form
    Added Popper to the Handicap name, so hovering it will show the handicap options. also the handicap and the configured PCs are showing in a table now so it is more easy to get info from it.

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from network_ui import app
+from network_ui import app, db
 from flask import render_template
 from network_ui.ip_addresses.models import IPAddress
+from network_ui.nw_handicaps.models import NetworkHandicap
 
 
 @app.route("/")
 def index():
     ip_addresses = IPAddress.query.all()
-    return render_template("home.html", ip_addresses=ip_addresses)
+    all_templates = NetworkHandicap.query.all()
+
+    templates = {}
+    for template in all_templates:
+        templates[template.handicap_name] = {
+            "pc": (
+                db.session.query(IPAddress, NetworkHandicap)
+                .select_from(IPAddress)
+                .join(NetworkHandicap)
+                .filter(IPAddress.network_handicap == template.id)
+                .all()
+            ),
+            "template": template,
+        }
+        print(template.bandwidth_restriction_upload)
+    return render_template("home.html", templates=templates)
 
 
 if __name__ == "__main__":
