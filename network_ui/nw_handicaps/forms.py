547733997dd6f284aa9bde2540cb4e875b3e7ac0
AkberJag <akberjag@gmail.com>
2022-12-23 00:08:04 +0530

    Made the network handicap form fields optional

--- network_ui/nw_handicaps/forms.py
+++ network_ui/nw_handicaps/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, SubmitField, FloatField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Optional
 
 
 class AddForm(FlaskForm):
@@ -8,14 +8,16 @@ class AddForm(FlaskForm):
         "Name of handicap template:", validators=[InputRequired()]
     )
     bandwidth_restriction_upload = FloatField(
-        "Enter Bandwidth restriction - max upload:"
+        "Enter Bandwidth restriction - max upload:", validators=[Optional()]
     )
     bandwidth_restriction_download = FloatField(
-        "Enter Bandwidth restriction - max download:"
+        "Enter Bandwidth restriction - max download:", validators=[Optional()]
     )
-    dns_latency = FloatField("DNS Latency (In seconds):")
-    general_latency = FloatField("General Latency (In seconds):")
+    dns_latency = FloatField("DNS Latency (In seconds):", validators=[Optional()])
+    general_latency = FloatField(
+        "General Latency (In seconds):", validators=[Optional()]
+    )
 
-    packet_loss = FloatField("Packet Loss %:")
+    packet_loss = FloatField("Packet Loss %:", validators=[InputRequired()])
 
     submit = SubmitField("Add handicap template")
